ca6157c6d4f4b69a01367b5a3119b6d09b399b8f
Felipe Arruda Pontes <felipe.arruda.pontes@insight-centre.org>
2020-05-14 18:22:06 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='adaptation_monitor',
+    version='0.1',
+    description='Service responsible for the Monitoring aspect of the MAPE-K architecture for adaptation',
+    author='Felipe Arruda Pontes',
+    author_email='felipe.arruda.pontes@insight-centre.org',
+    packages=['adaptation_monitor'],
+    zip_safe=False
+)
