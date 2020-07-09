47f262fd45faa4d2caf535cb5fe8c3e0efb8fff3
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2020-07-09 11:58:00 +0100

    adding initial mocked action to start the monitoring of the specific services we need. In the future this info will be anounced by...not sure? discovery service? could be a good idea

--- adaptation_monitor/send_msgs_test.py
+++ adaptation_monitor/send_msgs_test.py
@@ -22,26 +22,47 @@ def new_action_msg(action, event_data):
 
 
 def send_action_msgs(service_cmd):
-    msg_1 = new_action_msg(
-        'addQuery',
-        {
-            'subscriber_id': '123456',
-            'query_num': '654321',
-            'query': "select object_detection from publisher 1 where (object.label = 'car') within TIMEFRAMEWINDOW(10) withconfidence >50"
-        }
-    )
-    # msg_2 = new_action_msg(
-    #     'someOtherAction',
+    # msg_1 = new_action_msg(
+    #     'addQuery',
     #     {
-    #         'some': 'other event',
-    #         'data': 'to be used'
+    #         'subscriber_id': '123456',
+    #         'query_num': '654321',
+    #         'query': "select object_detection from publisher 1 where (object.label = 'car') within TIMEFRAMEWINDOW(10) withconfidence >50"
     #     }
     # )
+    msg_2 = new_action_msg(
+        'repeatMonitorStreamsSize',
+        {
+            'repeat_after_time': 1,
+            'services': {
+                'ObjectDetection': {
+                    'workers': [
+                        {
+                            'stream_key': 'object-detection-ssd-gpu-data',
+                            'queue_limit': 100
+                        },
+                        {
+                            'stream_key': 'object-detection-ssd-data',
+                            'queue_limit': 100
+                        }
+                    ]
+                },
+                'ColorDetection': {
+                    'workers': [
+                        {
+                            'stream_key': 'color-detection-data',
+                            'queue_limit': 100
+                        }
+                    ]
+                }
+            }
+        }
+    )
 
-    print(f'Sending msg {msg_1}')
-    service_cmd.write_events(msg_1)
-    # print(f'Sending msg {msg_2}')
-    # service_cmd.write_events(msg_2)
+    # print(f'Sending msg {msg_1}')
+    # service_cmd.write_events(msg_1)
+    print(f'Sending msg {msg_2}')
+    service_cmd.write_events(msg_2)
 
 
 def send_data_msg(service_stream):
@@ -63,7 +84,8 @@ def main():
     service_stream = stream_factory.create(SERVICE_STREAM_KEY, stype='streamOnly')
     monitor_stream = stream_factory.create('cm-cmd', stype='streamOnly')
     import ipdb; ipdb.set_trace()
-    send_action_msgs(monitor_stream)
+    send_action_msgs(service_cmd)
+    # send_action_msgs(monitor_stream)
     # send_data_msg(service_stream)
 
 
--- adaptation_monitor/service.py
+++ adaptation_monitor/service.py
@@ -1,5 +1,7 @@
+import functools
 import hashlib
 import threading
+import time
 
 from event_service_utils.logging.decorators import timer_logger
 from event_service_utils.services.tracer import BaseTracerService, tags, EVENT_ID_TAG
@@ -36,6 +38,7 @@ class AdaptationMonitor(BaseTracerService):
         self.monitored_streams = self.stream_factory.create(key=self.monitored_stream_keys, stype='manyKeyConsumer')
         self.knowledge_cmd_stream = self.stream_factory.create(key=self.knowledge_cmd_stream_key, stype='streamOnly')
         self.analyser_cmd_stream = self.stream_factory.create(key=self.analyser_cmd_stream_key, stype='streamOnly')
+        self.base_namespace = 'gnosis-mep'
 
     @timer_logger
     def process_data_event(self, event_data, json_msg):
@@ -44,12 +47,14 @@ class AdaptationMonitor(BaseTracerService):
         # do something here
         pass
 
-    def send_data_to_knowledge_manager(self, json_ld_entity, action):
+    def send_data_to_knowledge_manager(self, json_ld_entity, action, replaces=None):
         new_event_data = {
             'id': self.service_based_random_event_id(),
             'entity': json_ld_entity,
             'action': action
         }
+        if replaces:
+            new_event_data['replaces'] = replaces
 
         self.logger.debug(f'Sending data "{new_event_data}" to K')
         self.write_event_with_trace(new_event_data, self.knowledge_cmd_stream)
@@ -65,14 +70,16 @@ class AdaptationMonitor(BaseTracerService):
         self.logger.debug(f'Sending data "{new_event_data}" to Analyser')
         self.write_event_with_trace(new_event_data, self.analyser_cmd_stream)
 
+    def get_namespace_attribute(self, entity_namespace, attribute):
+        return f'{entity_namespace}#{attribute}'
+
     def prepare_entity_to_knowledge_manager(self, event_data, namespace, entity_id):
         clean_event_data = event_data.copy()
         clean_event_data.pop('id', None)
         clean_event_data.pop('tracer', None)
-        base_namespace = 'gnosis-mep'
-        entity_namespace = f'{base_namespace}:{namespace}'
+        entity_namespace = f'{self.base_namespace}:{namespace}'
         entity = {
-            '@id': f'{base_namespace}:{namespace}/{entity_id}',
+            '@id': f'{self.base_namespace}:{namespace}/{entity_id}',
             '@type': entity_namespace
         }
         entity.update({
@@ -100,9 +107,72 @@ class AdaptationMonitor(BaseTracerService):
 
         self.send_data_to_analyser(json_ld_entity, change_type=entity_action_change_type)
 
+    def _repeat_action_after_time(self, event_data, wait_time):
+        self.logger.debug(f'Waiting for {wait_time}s before repeating action...')
+        time.sleep(wait_time)
+
+        self.logger.debug(f'Done waiting, resending event to self cmd stream to repeat action: {event_data}')
+        # not using traces on purpose, I think this wont be much usefull for us
+        # and it may cause a very wied gigantic trace on jaeger
+        self.service_cmd.write_events(self.default_event_serializer(event_data))
+
+    def background_schedule_repeat_action(self, event_data, repeat_after_time):
+        bg_thread = threading.Thread(target=self._repeat_action_after_time, args=(event_data, repeat_after_time))
+        bg_thread.daemon = True
+        bg_thread.start()
+
+    @functools.lru_cache(maxsize=5)
+    def get_cached_stream_connection(self, stream_key):
+        return self.stream_factory.create(stream_key, stype='streamOnly')
+
+    def calculate_stream_len(self, stream_key):
+        stream = self.get_cached_stream_connection(stream_key)
+        return stream.single_io_stream.length()
+
+    def process_stream_size_monitoring(self, event_data):
+        services = event_data['services']
+        service_monitoring_entities = []
+        replaces = {}
+        for service_type, service in services.items():
+            workers = service['workers']
+            for worker in workers:
+                stream_key = worker['stream_key']
+                queue_limit = worker['queue_limit']
+                queue_size = self.calculate_stream_len(stream_key)
+                # queue_size = 10
+                queue_space = queue_limit - queue_size
+                queue_space_percent = queue_space / queue_limit
+                worker['queue_space'] = queue_space
+                worker['queue_space_percent'] = queue_space_percent
+                json_ld_entity = self.prepare_entity_to_knowledge_manager(
+                    worker, namespace='service_worker', entity_id=stream_key
+                )
+                # we want to replace the queue_space and percent values
+                # since they should be different and we don't want to have a list of values
+                # for this attribute
+                replaces[json_ld_entity['@id']] = [
+                    self.get_namespace_attribute(json_ld_entity['@type'], 'queue_space'),
+                    self.get_namespace_attribute(json_ld_entity['@type'], 'queue_space_percent'),
+                ]
+                service_monitoring_entities.append(json_ld_entity)
+
+        json_ld_entity_graph = {
+            '@graph': service_monitoring_entities
+        }
+        self.send_data_to_knowledge_manager(json_ld_entity_graph, action='updateEntity', replaces=replaces)
+
+    def monitor_stream_size_and_repeat(self, event_data):
+        self.process_stream_size_monitoring(event_data)
+        repeat_after_time = event_data['repeat_after_time']
+        self.background_schedule_repeat_action(event_data, repeat_after_time)
+
+    @timer_logger
     def process_action(self, action, event_data, json_msg):
         if not super(AdaptationMonitor, self).process_action(action, event_data, json_msg):
             return False
+
+        if action == 'repeatMonitorStreamsSize':
+            self.monitor_stream_size_and_repeat(event_data)
         # elif action == 'otherAction':
         #     # do some other action
         #     pass
@@ -163,6 +233,38 @@ class AdaptationMonitor(BaseTracerService):
         super(AdaptationMonitor, self).log_state()
         self.logger.info(f'My service name is: {self.name}')
 
+    def mocked_services_anouncement_for_stream_check(self):
+        "Using this while we don't have a discovery service that will anounce this info"
+        time.sleep(1)
+        event_data = {
+            'id': self.service_based_random_event_id(),
+            'action': 'repeatMonitorStreamsSize',
+            'repeat_after_time': 1,
+            'services': {
+                'ObjectDetection': {
+                    'workers': [
+                        {
+                            'stream_key': 'object-detection-ssd-gpu-data',
+                            'queue_limit': 100
+                        },
+                        {
+                            'stream_key': 'object-detection-ssd-data',
+                            'queue_limit': 100
+                        }
+                    ]
+                },
+                'ColorDetection': {
+                    'workers': [
+                        {
+                            'stream_key': 'color-detection-data',
+                            'queue_limit': 100
+                        }
+                    ]
+                }
+            }
+        }
+        self.service_cmd.write_events(self.default_event_serializer(event_data))
+
     def run(self):
         super(AdaptationMonitor, self).run()
         self.cmd_thread = threading.Thread(target=self.run_forever, args=(self.process_cmd,))
@@ -172,6 +274,7 @@ class AdaptationMonitor(BaseTracerService):
         self.data_thread.start()
         self.monitoring_thread.start()
 
+        self.mocked_services_anouncement_for_stream_check()
         self.cmd_thread.join()
         self.data_thread.join()
         self.monitoring_thread.join()
