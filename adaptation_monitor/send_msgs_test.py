c49e4d28bb4c3f8bc240c66ed64c368b1da52c2a
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2021-11-29 18:10:52 +0000

    adding some msgs to manual test

--- adaptation_monitor/send_msgs_test.py
+++ adaptation_monitor/send_msgs_test.py
@@ -24,48 +24,49 @@ def new_msg(event_data):
 def main():
     stream_factory = RedisStreamFactory(host=REDIS_ADDRESS, port=REDIS_PORT)
     addworker_cmd = stream_factory.create(LISTEN_EVENT_TYPE_SERVICE_WORKER_ANNOUNCED, stype='streamOnly')
-    addworker_cmd.write_events(
-        new_msg(
-            {
-                'worker': {
-                    'service_type': 'ObjectDetection',
-                    'stream_key': 'objworker-key',
-                    'queue_limit': 100,
-                    'throughput': 10,
-                    'accuracy': 0.1,
-                    'energy_consumption': 100,
-                }
-            }
-        )
-    )
-    addworker_cmd.write_events(
-        new_msg(
-            {
-                'worker': {
-                    'service_type': 'ObjectDetection',
-                    'stream_key': 'objworker-key2',
-                    'queue_limit': 100,
-                    'throughput': 1,
-                    'accuracy': 0.9,
-                    'energy_consumption': 10,
-                }
-            }
-        )
-    )
-    addworker_cmd.write_events(
-        new_msg(
-            {
-                'worker': {
-                    'service_type': 'ColorDetection',
-                    'stream_key': 'clrworker-key',
-                    'queue_limit': 100,
-                    'throughput': 1,
-                    'accuracy': 0.9,
-                    'energy_consumption': 10,
-                }
-            }
-        )
-    )
+    workermon_stream = stream_factory.create('ServiceWorkersStreamMonitored', stype='streamOnly')
+    # addworker_cmd.write_events(
+    #     new_msg(
+    #         {
+    #             'worker': {
+    #                 'service_type': 'ObjectDetection',
+    #                 'stream_key': 'objworker-key',
+    #                 'queue_limit': 100,
+    #                 'throughput': 10,
+    #                 'accuracy': 0.1,
+    #                 'energy_consumption': 100,
+    #             }
+    #         }
+    #     )
+    # )
+    # addworker_cmd.write_events(
+    #     new_msg(
+    #         {
+    #             'worker': {
+    #                 'service_type': 'ObjectDetection',
+    #                 'stream_key': 'objworker-key2',
+    #                 'queue_limit': 100,
+    #                 'throughput': 1,
+    #                 'accuracy': 0.9,
+    #                 'energy_consumption': 10,
+    #             }
+    #         }
+    #     )
+    # )
+    # addworker_cmd.write_events(
+    #     new_msg(
+    #         {
+    #             'worker': {
+    #                 'service_type': 'ColorDetection',
+    #                 'stream_key': 'clrworker-key',
+    #                 'queue_limit': 100,
+    #                 'throughput': 1,
+    #                 'accuracy': 0.9,
+    #                 'energy_consumption': 10,
+    #             }
+    #         }
+    #     )
+    # )
 
     # repeat_cmd = stream_factory.create(LISTEN_EVENT_TYPE_REPEAT_MONITOR_STREAMS_SIZE_REQUESTED, stype='streamOnly')
 
@@ -100,8 +101,8 @@ def main():
     # )
 
     import ipdb; ipdb.set_trace()
+    # events = workermon_stream.read_events()
     worker_stream = stream_factory.create('objworker-key', stype='streamOnly')
-
     for i in range(10):
         worker_stream.write_events(
             new_msg(
